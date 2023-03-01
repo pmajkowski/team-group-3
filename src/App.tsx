9716d9ac0373906a980f4a6fc9529886b8695d26
X-Tripen <bnhp@op.pl>
2023-03-01 19:12:59 +0100

    added firebase connection && created simple test form input Signed-off-by: X-Tripen <bnhp@op.pl>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useRef, FormEvent } from "react"; 
+import { firestore } from "./firebase" 
+import { addDoc, collection } from "@firebase/firestore"
 import "./App.module.css";
 
-function App() {
+function App(): JSX.Element { 
+  const testRef = useRef<HTMLInputElement>(null);
+  const ref = collection(firestore, "test");
+  // tworzymy referencję do kolekcji w Firebase
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => { 
+    e.preventDefault();
+    // zapobiegamy domyślnej akcji formularza (wysłania danych)
+    let data = {
+      dataName: testRef.current?.value
+      // tworzymy obiekt z danymi do zapisania w Firebase
+    }
+
+    try {
+      await addDoc(ref, data); 
+    } catch (e) {
+      console.log(e); 
+    }
+    // zapisujemy dane w Firebase / wyświetlamy błędy w konsoli
+  };
+
   return (
-    <>
-      <div className="App">
-        Nasz wspólny projekt - A Team
-        <div>Kolejna linijka</div>
-      </div>
-      ;
-    </>
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={testRef} />
+        <button type="submit">submit to database</button>
+      </form>
+    </div>
   );
 }
 
