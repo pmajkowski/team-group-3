1ceb3e8f761a0604e284d4d0f57c7ad94b7eb9a8
pmajkowski <majkowski_20@o2.pl>
2023-03-11 17:35:23 +0100

    Footer created

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,19 @@
+import './footer.css'
+const Footer = () => {
+    return <div className="footer">
+        <section className="section-1">
+    <div>EQUIPMENT RENTALS</div>
+    <div>CONTACT US</div>
+    <div>
+        LOCATION
+        <div>Ul. Gołębia 3, 81-185 Gdynia</div>
+    </div>
+    </section>
+    <section className="section-2">
+<p>Copyright ©2023 Team-group-3, Inc. All Rights Reserved.</p>
+<p>Site by Hubert Paprocki, Przemysław Pietkun, Krzysztof Chorąży, Piotr Majkowski</p>
+    </section>
+    </div>
+}
+
+export default Footer;
