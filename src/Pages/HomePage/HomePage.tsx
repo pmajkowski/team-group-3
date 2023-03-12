611576c2e51c1643ca3e827bacf84d9b2ddb1146
silentmaster82 <przemyslaw.pietkun@gmail.com>
2023-03-12 14:43:41 +0000

    ContentForm & new image

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -7,42 +7,44 @@ import QuestionForm from "../../components/Questions/QuestionForm";
 import AboutForm from "../../components/about/AboutForm";
 import NavigationList from "../../components/Navigation/NavigationList";
 import UserNavigationList from "../../components/UserNavigation/UserNavigationList";
+import ContentForm from "../../components/content/ContentForm";
 
 function HomePage(): JSX.Element {
-	const testRef = useRef<HTMLInputElement>(null);
-	const ref = collection(firestore, "test");
-	// tworzymy referencję do kolekcji w Firebase
+  const testRef = useRef<HTMLInputElement>(null);
+  const ref = collection(firestore, "test");
+  // tworzymy referencję do kolekcji w Firebase
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		// zapobiegamy domyślnej akcji formularza (wysłania danych)
-		let data = {
-			dataName: testRef.current?.value,
-			// tworzymy obiekt z danymi do zapisania w Firebase
-		};
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // zapobiegamy domyślnej akcji formularza (wysłania danych)
+    let data = {
+      dataName: testRef.current?.value,
+      // tworzymy obiekt z danymi do zapisania w Firebase
+    };
 
-		try {
-			await addDoc(ref, data);
-		} catch (e) {
-			console.log(e);
-		}
-		// zapisujemy dane w Firebase / wyświetlamy błędy w konsoli
-	};
+    try {
+      await addDoc(ref, data);
+    } catch (e) {
+      console.log(e);
+    }
+    // zapisujemy dane w Firebase / wyświetlamy błędy w konsoli
+  };
 
-	return (
-		<div className="App">
-			<UserNavigationList />
-			<NavigationList />
-			<form onSubmit={handleSubmit}>
-				<input type="text" ref={testRef} />
-				<Button type={"submit"} primary>
-					submit to database
-				</Button>
-				<QuestionForm></QuestionForm>
-				<AboutForm></AboutForm>
-			</form>
-		</div>
-	);
+  return (
+    <div className="App">
+      <UserNavigationList />
+      <NavigationList />
+      <form onSubmit={handleSubmit}>
+        <input type="text" ref={testRef} />
+        <Button type={"submit"} primary>
+          submit to database
+        </Button>
+        <QuestionForm></QuestionForm>
+        <ContentForm></ContentForm>
+        <AboutForm></AboutForm>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
