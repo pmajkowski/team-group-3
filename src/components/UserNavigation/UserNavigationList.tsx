45cb345a401726f8cbbe3ec7cb3029f775011338
X-Tripen <bnhp@op.pl>
2023-03-12 15:54:58 +0100

    usernavigation- ai comments

--- src/components/UserNavigation/UserNavigationList.tsx
+++ src/components/UserNavigation/UserNavigationList.tsx
@@ -1,11 +1,14 @@
+// Import the UserNavigationItem component from the "./UserNavigationItem" file
 import UserNavigationItem from "./UserNavigationItem";
 
+// Define an interface for the UserNavigation object, which has an id, name, and link property
 interface UserNavigation {
 	id: number;
 	name: string;
 	link: string;
 }
 
+// Define an array of UserNavigation objects with data for each navigation item
 const userNavigationList: UserNavigation[] = [
 	{
 		id: 1,
@@ -19,8 +22,11 @@ const userNavigationList: UserNavigation[] = [
 	},
 ];
 
+// Define a React component called UserNavigationList that renders a list of user navigation items
 function UserNavigationList() {
+	// Use the map method to iterate over the userNavigationList array and create a UserNavigationItem component for each item
 	const renderedNavigationList = userNavigationList.map((userNavigation) => {
+		// Pass the UserNavigationItem component the userNavigation object's id, name, and link properties as props
 		return (
 			<UserNavigationItem
 				key={userNavigation.id}
@@ -29,6 +35,7 @@ function UserNavigationList() {
 			/>
 		);
 	});
+	// Return a div that contains the renderedNavigationList of UserNavigationItem components
 	return (
 		<div className="flex flex-wrap justify-center items-center max-w-6xl mx-auto my-12 flex-row gap-x-5">
 			{renderedNavigationList}
@@ -36,4 +43,5 @@ function UserNavigationList() {
 	);
 }
 
+// Export the UserNavigationList component as the default export of the file
 export default UserNavigationList;
