36747f82ba7e2f089fba3f1889328bc5c8394440
serchristof <krzysztof.chorazy88@gmail.com>
2023-03-18 21:12:54 +0100

    feat: add location marker to leaflet map

--- src/components/Maps/Maps.tsx
+++ src/components/Maps/Maps.tsx
@@ -1,11 +1,24 @@
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "./leaflet.css";
+import L from "leaflet";
+
+const myIcon = L.icon({
+  iconUrl: require("./locationPointer.png"),
+  iconSize: [48, 48],
+  // iconAnchor: [15, 15],
+  // popupAnchor: [0, -15],
+  // shadowUrl: 'path/to/my/icon-shadow.png',
+  // shadowSize: [30, 30],
+  // shadowAnchor: [15, 15]
+});
 
 export default function Maps() {
+  const position: [number, number] = [54.5, 18.5];
+
   return (
     <div className="leaflet-container">
       <MapContainer
-        center={[54.5, 18.5]}
+        center={position}
         zoom={13}
         scrollWheelZoom={true}
         maxBoundsViscosity={1.0}
@@ -14,10 +27,8 @@ export default function Maps() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[54.5, 18.5]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={position} icon={myIcon}>
+          <Popup>Tu jest nasza siedziba</Popup>
         </Marker>
       </MapContainer>
     </div>
