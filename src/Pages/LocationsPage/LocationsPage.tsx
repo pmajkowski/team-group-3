38383fbf00bf79698ce0a93da6a570eda1a56c5c
serchristof <krzysztof.chorazy88@gmail.com>
2023-03-26 21:34:05 +0200

    #3_KCH - LocationPage - dodanie UserNavigation,NavigationList,LocationContainer,Footer

--- src/Pages/LocationsPage/LocationsPage.tsx
+++ src/Pages/LocationsPage/LocationsPage.tsx
@@ -1,8 +1,17 @@
-import React from "react";
-import SidebarNavigationList from "../../components/SidebarNavigation/SidebarNavigationList";
+import UserNavigationList from "../../components/UserNavigation/UserNavigationList";
+import NavigationList from "../../components/Navigation/NavigationList";
+import LocationContainer from '../../components/Locations/LocationContainer'
+import Footer from "../../components/footer/footer";
 
-function LocationsPage() {
-	return <SidebarNavigationList />;
+function LocationsPage(): JSX.Element {
+  return (
+    <div>
+      <UserNavigationList />
+      <NavigationList />
+			<LocationContainer/>
+      <Footer />
+    </div>
+  );
 }
 
 export default LocationsPage;
---
+++ src/components/Locations/LocationContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./LocationContainer.module.css";
+
+const LocationContainer = (): JSX.Element => {
+  return <div className={classes.container}>LocationPoints</div>;
+};
+
+export default LocationContainer;
