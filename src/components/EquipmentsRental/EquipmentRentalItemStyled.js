ecfc96262eac89b7052a18ef74a5b74dec9f6ca5
X-Tripen <bnhp@op.pl>
2023-03-28 22:40:13 +0200

    fix:change styling for equipmentsrentallist

--- src/components/EquipmentsRental/EquipmentRentalItemStyled.js
+++ src/components/EquipmentsRental/EquipmentRentalItemStyled.js
@@ -1,32 +1,41 @@
 import styled from "styled-components";
 
 export const SingleImageClass = styled.div`
-    padding: 10px;
-    border-radius: 10px;
-    background-color:azure; 
-    
-`
+  padding: 10px;
+  border-radius: 10px;
+  background-color: azure;
+  display: flex;
+  flex-direction: column;
+  max-width: 225px;
+`;
 export const ImageEffectClass = styled.div`
-    transition: transform 0.2s ease-in-out;
-    border-radius: 10px;
-    &:hover {
-        transform: scale(0.9); 
-    }
-`
+  transition: transform 0.2s ease-in-out;
+  border-radius: 10px;
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+  overflow: hidden;
+  height: 100%;
+  &:hover {
+    transform: scale(0.9);
+  }
+`;
 export const EquipmentData = styled.div`
-width: 200px;
-li:first-child {
+  li {
+    list-style: none;
+    text-align: center;
+  }
+  li:first-child {
     font-weight: 700;
     margin-bottom: 10px;
-}
-li:last-child {
+  }
+  li:last-child {
     border-top: 1px dotted black;
     margin-top: 20px;
     font-size: 12px;
-}
-li {
-
-    overflow: hidden;
-}
-
-`
+  }
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 50%;
+`;
--- src/components/EquipmentsRental/EquipmentsRentalItem.tsx
+++ src/components/EquipmentsRental/EquipmentsRentalItem.tsx
@@ -1,31 +1,35 @@
-import a1 from "../../img/equipments/excavator attachments/a1.jpeg"
-import a2 from "../../img/equipments/excavator attachments/a2.jpeg"
-import o1 from "../../img/equipments/general construction equipments/o1.jpeg"
-import o2 from "../../img/equipments/general construction equipments/o2.jpeg"
-import o3 from "../../img/equipments/general construction equipments/o3.jpeg"
-import w1 from "../../img/equipments/high performance machines/w1.jpeg"
-import w2 from "../../img/equipments/high performance machines/w2.jpeg"
-import w3 from "../../img/equipments/high performance machines/w3.jpeg"
-import w4 from "../../img/equipments/high performance machines/w4.jpeg"
-import w5 from "../../img/equipments/high performance machines/w5.jpeg"
-import t1 from "../../img/equipments/rail transport/t1.jpeg"
-import t2 from "../../img/equipments/rail transport/t2.jpeg"
-import zg1 from "../../img/equipments/rail welding/zg1.jpeg"
-import z1 from "../../img/equipments/turnout installation/z1.jpeg"
-import z2 from "../../img/equipments/turnout installation/z2.jpeg"
-import { EquipmentData, ImageEffectClass, SingleImageClass } from "./EquipmentRentalItemStyled"
+import a1 from "../../img/equipments/excavator attachments/a1.jpeg";
+import a2 from "../../img/equipments/excavator attachments/a2.jpeg";
+import o1 from "../../img/equipments/general construction equipments/o1.jpeg";
+import o2 from "../../img/equipments/general construction equipments/o2.jpeg";
+import o3 from "../../img/equipments/general construction equipments/o3.jpeg";
+import w1 from "../../img/equipments/high performance machines/w1.jpeg";
+import w2 from "../../img/equipments/high performance machines/w2.jpeg";
+import w3 from "../../img/equipments/high performance machines/w3.jpeg";
+import w4 from "../../img/equipments/high performance machines/w4.jpeg";
+import w5 from "../../img/equipments/high performance machines/w5.jpeg";
+import t1 from "../../img/equipments/rail transport/t1.jpeg";
+import t2 from "../../img/equipments/rail transport/t2.jpeg";
+import zg1 from "../../img/equipments/rail welding/zg1.jpeg";
+import z1 from "../../img/equipments/turnout installation/z1.jpeg";
+import z2 from "../../img/equipments/turnout installation/z2.jpeg";
+import {
+  EquipmentData,
+  ImageEffectClass,
+  SingleImageClass,
+} from "./EquipmentRentalItemStyled";
 interface EquipmentsDetails {
-    name:string,
-    price:number,
-    rentalType:string,
-    availability: boolean,
-    category:string,
-    src:string,
-    alt:string
+  name: string;
+  price: number;
+  rentalType: string;
+  availability: boolean;
+  category: string;
+  src: string;
+  alt: string;
 }
 export interface EquipmentImageItemDetails {
-  src:string;
-  alt:string;
+  src: string;
+  alt: string;
 }
 
 // export interface EquipmentImageItemProps {
@@ -34,21 +38,21 @@ export interface EquipmentImageItemDetails {
 
 // export type EquipmentsDetailsAndItemDetails = EquipmentsDetails & EquipmentImageItemProps;
 
-export function EquipmentsRentalItem(props:EquipmentsDetails) {
-  console.log(props.src)
+export function EquipmentsRentalItem(props: EquipmentsDetails) {
+  console.log(props.src);
   return (
     <SingleImageClass>
       <ImageEffectClass>
         <img src={props.src} alt={props.alt} />
       </ImageEffectClass>
-    <EquipmentData>
-      <ul>
+      <EquipmentData>
         <li>{props.name} </li>
-        <li>Price: {props.price}zł/{props.rentalType}</li>
-        <li>Availability: {props.availability === true?"Yes":"No"}</li>
+        <li>
+          Price: {props.price}zł/{props.rentalType}
+        </li>
+        <li>Availability: {props.availability === true ? "Yes" : "No"}</li>
         <li>{props.category}</li>
-      </ul>
-    </EquipmentData>
+      </EquipmentData>
     </SingleImageClass>
-  )
+  );
 }
