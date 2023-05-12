91ede16267275cd68577192f41e1452a60911252
silentmaster82 <przemyslaw.pietkun@gmail.com>
2023-05-12 20:30:56 +0100

    polish train

--- src/components/content/ContentForm.jsx
+++ src/components/content/ContentForm.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "./ContentForm.css";
-import videoBg from "./../../movies/train2.mp4";
+import videoBg from "./../../movies/train.mp4";
 
 const ContentForm = () => {
   return (
-    // <div className="container_tiles">
-    //   <div className="tile">
-    //     <img src={trainImg} alt="Train on the track" />
-    //     <div className="h2_tile">1st train</div>
-    //   </div>
-    // </div>
     <div className="container_tiles">
       <video src={videoBg} autoPlay muted loop />
     </div>
